a22cf1d76efef20e36ab5068aee8a99ca483b8da
unknown <okuracah@gmail.com>
2024-08-09 17:00:00 -0300

    Create Repository for Lesson and First Lesson

---
+++ IntroductionToC#/Sine Cosine/Sine Cosine/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sine_Cosine
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Set your angle");
+            int angle =Convert.ToInt32(Console.ReadLine());
+
+            float degree = (float) angle;
+            float radians = degree * (float)Math.PI / 180; 
+
+            float sine = (float) Math.Sin (radians);
+            float cosine = (float) Math.Cos(radians);
+
+            Console.WriteLine("The sine of " + degree + "º is " + sine);
+            Console.WriteLine("The cosine of " + degree + "º is " + cosine);
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# C-Projects
+C# and Unity exercises 
+
+## Introduction to C#
+
+# Sine Cosine 
+- Create variable 
+- Typecast value
+- Using C# Documantation
